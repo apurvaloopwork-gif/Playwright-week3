110426a39abca82b69fee638966d842292890d76
apurvaloopwork-gif <apurva.loopwork@gmail.com>
2026-06-23 09:21:44 +0530

    mini build added

--- mini_build_playwright.py
+++ mini_build_playwright.py
@@ -1,16 +1,21 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as playwright:
-  browser = playwright.chromium.launch(headless = False,slow_mo=500)
-  page = browser.new_page()
-  
-  page.goto("https://www.google.com/maps")
-  page.wait_for_timeout(2000)
-  
-  serach_box = page.get_by_label("Search Google Maps")
-  serach_box.fill("gym mumbai Andheri")
-  page.keyboard.press("Enter")
-  page.wait_for_timeout(2000)
-  
-  
-  browser.close() 
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    page = browser.new_page()
+
+    page.goto("https://www.google.com/maps")
+
+    search_box = page.get_by_label("Search Google Maps")
+    search_box.fill("gym mumbai Andheri")
+    page.keyboard.press("Enter")
+
+    articles = page.get_by_role("article")
+    articles.first.wait_for()
+    name = page.locator("a[aria-label]").first
+    print(name)
+    print("Total gyms =", articles.count())
+
+    page.wait_for_timeout(5000)
+
+    browser.close()
